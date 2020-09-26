c38633fa77a1c4b96a5d95f49d015d420c7e2620
zhuoqi-chen <zhuoqi_chen@126.com>
2020-09-26 18:08:11 +0800

    使用泛型

--- 2.2/1.ts
+++ 2.2/1.ts
@@ -1,6 +1,6 @@
 const data = [12, 34, 53, 23, 23, 34, 235, 342345, 3, 16, 43, 7, 43];
 
-function findIndex(data: number[], target: number) {
+function findIndex<T>(data: T[], target: T) {
   for (let index = 0; index < data.length; index++) {
     const curr = data[index];
     if (curr === target) {
