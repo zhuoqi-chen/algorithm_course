af35da0fa8a405f9864fddbb2198c590c21b0d1a
zhuoqi-chen <zhuoqi_chen@126.com>
2020-09-26 20:12:21 +0800

    支持自定义判断

--- 2.2/1.spec.ts
+++ 2.2/1.spec.ts
@@ -1,8 +1,31 @@
 import { findIndex } from "./1";
+class Student {
+  constructor(public name: string) {}
+  public equals(student?: Student): boolean {
+    if (student === undefined) {
+      return false;
+    }
+    if (student === this) {
+      return true;
+    }
+    if (student instanceof Student && this.name === student.name) {
+      return true;
+    }
+    return false;
+  }
+}
 
 describe("2.2", () => {
-  it("test find index", () => {
-    const data = [12, 34, 53, 23, 23, 34, 235, 342345, 3, 16, 43, 7, 43];
+  it("test string", () => {
+    const data = ["12", "34", "53", "23"];
     expect(findIndex(data, data[3])).toBe(3);
+  });
+  it("test number", () => {
+    const data = [12, 34, 53, 23];
+    expect(findIndex(data, data[3])).toBe(3);
+  });
+  it("test object", () => {
+    const data = [new Student("czq"), new Student("ty")];
+    expect(findIndex(data, new Student("czq"))).toBe(0);
   });
 });
--- 2.2/1.ts
+++ 2.2/1.ts
@@ -1,7 +1,19 @@
-export function findIndex<T>(data: T[], target: T) {
+export function findIndex<T extends { equals: (target: T) => boolean }>(
+  data: T[] | number[] | string[],
+  target: T | number | string
+) {
   for (let index = 0; index < data.length; index++) {
     const curr = data[index];
     if (curr === target) {
+      return index;
+    }
+    if (
+      typeof curr === "object" &&
+      typeof target === "object" &&
+      "equals" in curr &&
+      "equals" in target &&
+      curr.equals(target)
+    ) {
       return index;
     }
   }
