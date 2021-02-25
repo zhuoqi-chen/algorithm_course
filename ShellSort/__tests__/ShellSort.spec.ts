5ea740285aa6fa89d6b76c2ec58c6635fc569887
zhuoqi.chen <zhuoqi_chen@126.com>
2021-02-25 23:16:11 +0800

    test: test yarn cache

--- ShellSort/__tests__/ShellSort.spec.ts
+++ ShellSort/__tests__/ShellSort.spec.ts
@@ -3,7 +3,7 @@ import { ShellSort } from "../ShellSort";
 const sortFn = (a: number, b: number) => (a - b > 0 ? 1 : -1);
 describe("test ShellSort", () => {
   it("sort", () => {
-    const input = generateNumberArr(100);
+    const input = generateNumberArr(110);
     const output = input.slice();
     expect(ShellSort.sort(input)).toEqual(output.sort(sortFn));
   });
