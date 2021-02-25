0feb984895815311256bc951f0a7d90c987de62d
zhuoqi.chen <zhuoqi_chen@126.com>
2021-02-25 19:06:51 +0800

    feat: 冒牌排序优化

--- BubbleSort/BubbleSort.ts
+++ BubbleSort/BubbleSort.ts
@@ -1,16 +1,48 @@
 export class BubbleSort {
   static sort(arr: number[]) {
     const length = arr.length;
-    for (let i = 0; i + 1 < arr.length; i++) {
-      for (let j = 0; j <= length - i - 1; j++) {
+    for (let i = 0; i + 1 < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
-          BubbleSort.swag(arr, i, j + 1);
+          BubbleSort.swap(arr, j, j + 1);
         }
       }
     }
     return arr;
   }
-  static swag(arr: number[], i: number, j: number) {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+  static sortOptimize1(arr: number[]) {
+    const length = arr.length;
+    for (let i = 0; i + 1 < length; i++) {
+      let isSwapped = false;
+      for (let j = 0; j < length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          isSwapped = true;
+          BubbleSort.swap(arr, j, j + 1);
+        }
+      }
+      if (!isSwapped) {
+        break;
+      }
+    }
+    return arr;
+  }
+  static sortOptimize2(arr: number[]) {
+    const length = arr.length;
+    for (let i = 0; i + 1 < length; ) {
+      let lastSwapped = 0;
+      for (let j = 0; j < length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          BubbleSort.swap(arr, j, j + 1);
+          lastSwapped = j + 1;
+        }
+      }
+      i = length - lastSwapped;
+    }
+    return arr;
+  }
+  static swap(arr: number[], i: number, j: number) {
+    const temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
   }
 }
--- BubbleSort/__tests__/BubbleSort.spec.ts
+++ BubbleSort/__tests__/BubbleSort.spec.ts
@@ -1,9 +1,20 @@
 import { generateNumberArr } from "../../Utils/util";
 import { BubbleSort } from "../BubbleSort";
-
+const sortFn = (a: number, b: number) => (a - b > 0 ? 1 : -1);
 describe("test BubbleSort", () => {
-  it("should ", () => {
+  it("sort", () => {
     const input = generateNumberArr(100);
-    expect(BubbleSort.sort(input)).toEqual(input.sort());
+    const output = input.slice();
+    expect(BubbleSort.sort(input)).toEqual(output.sort(sortFn));
+  });
+  it("sort sortOptimize1", () => {
+    const input = generateNumberArr(100);
+    const output = input.slice();
+    expect(BubbleSort.sortOptimize1(input)).toEqual(output.sort(sortFn));
+  });
+  it("sort sortOptimize2", () => {
+    const input = generateNumberArr(100);
+    const output = input.slice();
+    expect(BubbleSort.sortOptimize2(input)).toEqual(output.sort(sortFn));
   });
 });
