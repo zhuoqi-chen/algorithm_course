c3d6927476cd6ca968ea229c497bb51902a92fcd
zhuoqi.chen <zhuoqi_chen@126.com>
2021-02-25 21:43:17 +0800

    feat: update swap

--- BubbleSort/BubbleSort.ts
+++ BubbleSort/BubbleSort.ts
@@ -41,8 +41,6 @@ export class BubbleSort {
     return arr;
   }
   static swap(arr: number[], i: number, j: number) {
-    const temp = arr[j];
-    arr[j] = arr[i];
-    arr[i] = temp;
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
 }
