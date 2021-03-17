fe612470d4399853ac41cf222113fbc4eb916f1b
zhuoqi.chen <zhuoqi_chen@126.com>
2021-03-17 23:03:35 +0800

    feat: 线段树查询

--- SegmentTree/SegmentTree.ts
+++ SegmentTree/SegmentTree.ts
@@ -15,6 +15,47 @@ export class SegmentTree {
   public getTree() {
     return this.tree;
   }
+  public query(queryLeft: number, queryRight: number): number {
+    if (
+      queryLeft < 0 ||
+      queryRight >= this.getSize() ||
+      queryRight < queryLeft
+    ) {
+      throw new Error("index is illegal");
+    }
+    return this.queryR(0, 0, this.getSize() - 1, queryLeft, queryRight);
+  }
+  private queryR(
+    treeIndex: number,
+    l: number,
+    r: number,
+    queryLeft: number,
+    queryRight: number
+  ): number {
+    if (l === queryLeft && r === queryRight) {
+      return this.tree[treeIndex];
+    }
+    const mid = this.midNumber(l, r);
+    const leftTreeIndex = this.leftChild(treeIndex);
+    const rightTreeIndex = this.rightChild(treeIndex);
+    if (queryLeft >= mid + 1) {
+      return this.queryR(rightTreeIndex, mid + 1, r, queryLeft, queryRight);
+    } else if (queryRight <= mid) {
+      return this.queryR(leftTreeIndex, l, mid, queryLeft, queryRight);
+    }
+    const leftRt = this.queryR(leftTreeIndex, l, mid, queryLeft, mid);
+    const rightRt = this.queryR(
+      rightTreeIndex,
+      mid + 1,
+      r,
+      mid + 1,
+      queryRight
+    );
+    return this.mergerFn(leftRt, rightRt);
+  }
+  private midNumber(l: number, r: number) {
+    return Math.floor(l + (r - l) / 2);
+  }
   private buildSegmentTree(treeIndex: number, l: number, r: number) {
     if (l === r) {
       this.tree[treeIndex] = this.data[l];
@@ -22,7 +63,7 @@ export class SegmentTree {
     }
     const leftTreeIndex = this.leftChild(treeIndex);
     const rightTreeIndex = this.rightChild(treeIndex);
-    const mid = Math.floor(l + (r - l) / 2);
+    const mid = this.midNumber(l, r);
     this.buildSegmentTree(leftTreeIndex, l, mid);
     this.buildSegmentTree(rightTreeIndex, mid + 1, r);
     this.tree[treeIndex] = this.mergerFn(
--- SegmentTree/__tests__/SegmentTree.spec.ts
+++ SegmentTree/__tests__/SegmentTree.spec.ts
@@ -1,24 +1,58 @@
 import { MergerFn, SegmentTree } from "../SegmentTree";
-const caseList = [
-  {
-    input: [1],
-    output: [1],
-  },
-  {
-    input: [1, 2],
-    output: [3, 1, 2],
-  },
-  {
-    input: [1, 2, 3],
-    output: [6, 3, 3, 1, 2],
-  },
-];
+
 const add: MergerFn = (a, b) => a + b;
-describe.only("test SegmentTree", () => {
-  caseList.forEach(({ input, output }) => {
-    it(`test case [${input}] should return [${output}]`, () => {
-      const tree = new SegmentTree(input, add);
-      expect(tree.getTree()).toEqual(output);
+describe("test SegmentTree", () => {
+  describe("test tree result", () => {
+    const caseList = [
+      {
+        input: [1],
+        output: [1],
+      },
+      {
+        input: [1, 2],
+        output: [3, 1, 2],
+      },
+      {
+        input: [1, 2, 3],
+        output: [6, 3, 3, 1, 2],
+      },
+    ];
+    caseList.forEach(({ input, output }) => {
+      it(`test case [${input}] should return [${output}]`, () => {
+        const tree = new SegmentTree(input, add);
+        expect(tree.getTree()).toEqual(output);
+      });
+    });
+  });
+  describe("test query", () => {
+    const caseList = [
+      {
+        left: 0,
+        right: 0,
+        output: 1,
+      },
+      {
+        left: 0,
+        right: 1,
+        output: 3,
+      },
+      {
+        left: 2,
+        right: 4,
+        output: 12,
+      },
+      {
+        left: 4,
+        right: 4,
+        output: 5,
+      },
+    ];
+    const arr = [1, 2, 3, 4, 5];
+    caseList.forEach(({ left, right, output }) => {
+      it(`test query(${left},${right}) should return ${output}`, () => {
+        const tree = new SegmentTree(arr, add);
+        expect(tree.query(left, right)).toEqual(output);
+      });
     });
   });
 });
